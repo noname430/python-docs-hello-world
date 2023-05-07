1ec175ea909c9bebfaa4fdbc7dd81262959f1379
noname430 <56733462+noname430@users.noreply.github.com>
2023-05-07 16:05:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return ""
 
 @app.route("/20wk2yc8p6caztasx7bb")
 def hello_poc():
-    return "<!-- PoC -->"
+    return "<!-- subdomain takeover PoC by Dominique van Dorsselaer -->"
