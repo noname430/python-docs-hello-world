f6e9f4e8e81d15fec503f925a36f553a00d9942b
noname430 <56733462+noname430@users.noreply.github.com>
2023-05-07 21:31:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def hello_poc_sxss():
 @app.route('/6w1ymvcfdrf33n7m2cc6')
 def collect_cookies():
     CK = request.cookies.get('uMarketingSuiteAnalyticsVisitorId')
-    return CK 
+    return f"This is a {CK}" 
